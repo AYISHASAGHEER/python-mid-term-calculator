45817033199f254c93b083090c4bc0ae9931e55b
AYISHASAGHEER <156579204+AYISHASAGHEER@users.noreply.github.com>
2024-01-14 15:52:11 +0500

    Add files via upload

---
+++ o.py
@@ -0,0 +1,48 @@
+Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============ RESTART: C:/Users/gdu35/m.py ===========
+1.ADD
+2.SUBTRACT
+3.MULTIPLY
+4.DIVIDE
+
+============ RESTART: C:/Users/gdu35/m.py ===========
+1.ADD
+2.SUBTRACT
+3.MULTIPLY
+4.DIVIDE
+1
+Traceback (most recent call last):
+  File "C:/Users/gdu35/m.py", line 9, in <module>
+    if operations == "1":
+NameError: name 'operations' is not defined. Did you mean: 'opoperations'?
+>>> 
+========= RESTART: C:/Users/gdu35/m.py =========
+1.ADD
+2.SUBTRACT
+3.MULTIPLY
+4.DIVIDE
+
+======= RESTART: C:/Users/gdu35/m.py =======
+1.ADD
+2.SUBTRACT
+3.MULTIPLY
+4.DIVIDE
+
+======= RESTART: C:/Users/gdu35/m.py =======
+1.ADD
+2.SUBTRACT
+3.MULTIPLY
+4.DIVIDE
+4 first number: 20 
+Traceback (most recent call last):
+  File "C:/Users/gdu35/m.py", line 9, in <module>
+    if operations == "1":
+NameError: name 'operations' is not defined. Did you mean: 'opoperations'?
+>>> 
+======= RESTART: C:/Users/gdu35/m.py =======
+1.ADD
+2.SUBTRACT
+3.MULTIPLY
+4.DIVIDE
